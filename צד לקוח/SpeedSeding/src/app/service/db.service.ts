b73dc6bd81916a105e9673754dcdc1628dd99544
shirabura <burashira@gmail.com>
2022-02-08 17:35:10 +0200

    טפסים של התחברות והרשמה

--- צד לקוח/SpeedSeding/src/app/service/db.service.ts
+++ צד לקוח/SpeedSeding/src/app/service/db.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Users } from '../models/users';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DbService {
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
+
+  getUserDetails():Observable<Users[]>{
+    return this.httpClient.get<Users[]>('http://localhost:51067/api/USERS/GetallUsers');
+  }
+
+  getUserSignUp(){
+
+  }
 }
