adbfcbbed81479f55969fb30651595858881ca72
shirabura <95858971+shirabura@users.noreply.github.com>
2022-05-11 22:21:27 +0300

    יע

--- צד שרת/server/SpeedSeding/DAL/Model1.Designer.cs
+++ צד שרת/server/SpeedSeding/DAL/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\student\Desktop\Project\צד שרת\server\SpeedSeding\DAL\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Yael\Desktop\פרויקט שירה לא לגעתתת\שיט\צד שרת\server\SpeedSeding\DAL\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
